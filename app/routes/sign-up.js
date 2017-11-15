ef0e6345d9de24b42818590bcb3f22fc610295a5
Alvin Crespo <alvin.crespo@gmail.com>
2017-11-15 09:30:48 -0500

    Cleaning up some code

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,18 +1,21 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+const AUTHENTICATOR = 'authenticator:oauth2';
+
 export default Route.extend({
   session: service(),
 
   actions: {
     async onSignUp(email, password) {
-      let user = this.store.createRecord('user', { email, password });
+      let attrs = { email, password };
+      let user = this.store.createRecord('user', attrs);
 
       await user.save();
 
       let session = this.get('session');
 
-      await session.authenticate('authenticator:oauth2', email, password);
+      await session.authenticate(AUTHENTICATOR, email, password);
     }
   }
 });
